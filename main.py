4bde8febbb81680b5d031007eaa97694a8bc6348
Tomasz Kołtuniak <tomaszkoltuniak22@gmail.com>
2024-06-17 23:57:04 +0200

    Day 2, show company's facts

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from sec_data import show_all_companies
+from sec_data import get_all_companies, get_company_facts
 
 st.set_page_config(
     'Fundamentalytics',
@@ -8,10 +8,21 @@ st.set_page_config(
 
 company = st.selectbox(
     '',
-    show_all_companies(),
+    get_all_companies(),
     None,
     placeholder='Choose a company',
-    label_visibility='collapsed'
+    label_visibility='collapsed',
 )
 
-st.write('You selected:', company)
+if company is not None:
+    temp = company.split(' ')
+    ticker, title, cik_str = temp[0], ' '.join(temp[1:-1]), temp[-1]
+
+    st.write('# ', title)
+    st.write(ticker, cik_str)
+
+    all_facts = get_company_facts(cik_str)
+    for key, value in all_facts['facts'].items():
+        st.write('##' + value['label'])
+        st.write(value['unit'] + value['description'])
+        st.dataframe(value['data'])
--- sec_data.py
+++ sec_data.py
@@ -1,26 +1,67 @@
 import requests
 import json
+import os.path
+import pandas as pd
 
 header = {'User-Agent': 'Fundamentalytics'}
 
-def update_tickers(filename:str='all_tickers.json'):
+def download_tickers(filename:str='all_tickers.json'):
     url = 'https://www.sec.gov/files/company_tickers.json'
     response = requests.get(url, headers=header)
     with open(filename, 'w') as f:
         json.dump(response.json(), f)
 
 def load_tickers(filename:str='all_tickers.json'):
-    try:
-        with open(filename, 'r') as f:
-            all_tickers = json.load(f)
-    except:
-        update_tickers()
-        load_tickers()
+    if not os.path.isfile(filename):
+        download_tickers()
+    with open(filename, 'r') as f:
+        all_tickers = json.load(f)
     return all_tickers
 
-def show_all_companies():
+def get_all_companies():
     raw = load_tickers()
     all_companies = []
     for _, data in raw.items():
         all_companies.append(str(data['ticker'] + ' ' +  data['title'] + ' ' + str(data['cik_str'])))
     return all_companies
+
+def get_company_facts(cik: str):
+    if len(cik) < 10:
+        cik = (10 - len(cik)) * '0' + cik
+    
+    url = f'https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json'
+    response = requests.get(url, headers=header).json()
+    
+    result = {
+        'info': {
+            'cik': response['cik'],
+            'entityName': response['entityName']
+        },
+        'facts': {
+        }
+    }
+
+    facts = response['facts']['dei'] | response['facts']['us-gaap']
+    
+    for k, v in facts.items():
+        # date = []
+        # value = []
+        # for element in v['units'][str(list(v['units'].keys())[0])]:
+        #     if 'frame' in list(element.keys()) and element['form'] in ['10-Q', '10-K']:
+        #         date.append(str(element['fy']) + ' ' + element['fp'])
+        #         value.append(element['val'])
+
+        # temp_df = pd.DataFrame({'date': date, 'value': value})
+        # temp_df = temp_df.sort_values(by=['date'])
+
+        df = pd.DataFrame(v['units'][list(v['units'].keys())[0]])
+
+        temp_dict = {
+            'label': v['label'],
+            'description': v['description'],
+            'unit': list(v['units'].keys())[0],
+            'data': df
+        }
+        result['facts'][k] = temp_dict
+    del response
+    return result
